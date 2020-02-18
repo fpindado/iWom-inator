bcc0378bdb8a167abd70a6c36424bd9212f2c20e
osso73 <52031346+osso73@users.noreply.github.com>
2020-02-18 17:44:38 +0100

    Updated python, with different times for Fri, and multiple users

--- Python/README.md
+++ Python/README.md
@@ -1,15 +1,26 @@
 # iWom-inator
-iWom daily filling automation.<br>
-It only fills today information, not multi-day filling.<br>
+iWom daily filling automation. It only fills today information, not multi-day filling.
 
-## Documentation about XXXXX
--	xxxxx
--	xxxxx
+It allows for multiple user filling, using the login and passwords saved in a .csv file.
 
-## Installation
-1. xxxxx
-2. xxxxx
 
-## Execution
-1. xxxxx
-2. xxxxx
+## Installation and execution
+1. Get the latest release from python, under folder `releases\`.
+2. Unzip on a folder in your PC
+3. Update the file `users.csv` with your login(s) and password(s).
+4. Run file `iWom-update`.
+5. You can add the file in the windows scheduler, so it runs every day to update the time
+
+## Dependencies / issues
+
+This script uses [Selenium package](https://selenium.dev/) to automate the entry of hours, and it depends on having the right webdriver to interact with the browser. The script is ready to run on any of the browsers: Firefox, Chrome, Edge and Internet Explorer. The webdrivers for Firefox, Chrome and Edge are included in the distribution, but not for Edge, as its installation is quite dependant on the version; if you want to execute in Edge, you will need to install the latest webdriver from here:
+
+https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
+
+At the moment, it only works with Firefox. In later releases we will include launchers for each browser.
+
+In case you have issues with the webdriver that is included in the distribution, because using a different version of web browser, you can download the appropriate driver from here:
+
+https://selenium.dev/documentation/en/webdriver/driver_requirements/#quick-reference
+
+And place it under the same folder as the executable.
--- Python/iWom-update.py
+++ Python/iWom-update.py
@@ -5,72 +5,108 @@ Created on Mon Feb 17 18:15:06 2020
 @author: opujo
 """
 
-from selenium.webdriver import Firefox
+from selenium.webdriver import Firefox, Chrome, Ie, Edge
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from time import asctime, mktime, localtime
 
 CREDENTIALS = "users.csv"
 
-# calculate hours
-weekday = asctime()[:3]
-if weekday == 'Fri':
-    hours_value = '6:30'
-elif weekday in ['Mon', 'Tue', 'Wed', 'Thu']:
-    hours_value = '8:32'
-else:
-    print ("You can't run the macro during weekends!")
+def wait(sec):
+    t1 = mktime(localtime())
+    t2 = mktime(localtime())
+    while t2-t1 < sec:   # wait a number of seconds
+        t2 = mktime(localtime())
+    
+
+def calculate_hours(): # calculate hours
+    weekday = asctime()[:3]
+    if weekday == 'Fri':
+        h = '6:30'
+    elif weekday in ['Mon', 'Tue', 'Wed', 'Thu']:
+        h = '8:32'
+    else:
+        print ("You can't run the macro during weekends!")
+        h = '0:00'
+    return h
+
+def get_credentials():
+    # get credentials from file
+    with open (CREDENTIALS, encoding='utf-8-sig') as f:
+        credentials = f.readlines()
+    
+    users = dict()
+    for line in credentials:
+        if line[0] == '#':
+            continue
+        u, p = line.split(',')
+        users[u] = p.strip() # removing \n characters
+    return users
+    
 
 
-# get credentials from file
-with open (CREDENTIALS, encoding='utf-8-sig') as f:
-    credentials = f.readlines()
+class EnterHours:
+    def __init__(self, browser, username, userpwd, hours):
+        self.username = username
+        self.userpwd = userpwd
+        self.hours_value = hours
+        
+        self.open_session(browser)
+        self.login()
+        self.open_app()
+        self.entry_data()
+        self.save_quit()
+    
+    def open_session(self, browser):
+        if browser == 'firefox':
+            opts = Options()
+            opts.headless = True
+            assert opts.headless  # Operating in headless mode
+            self.session = Firefox(options=opts)
+        elif browser == 'ie':
+            self.session = Ie()
+        elif browser == 'chrome':
+            self.session = Chrome()
+        elif browser == 'edge':
+            self.session = Edge()
+    
+    def login(self):
+        self.session.get('https://www.bpocenter-dxc.com/iwom_web5/portal_apps.aspx')
+        self.session.find_element_by_id("LoginApps_UserName").send_keys(self.username)
+        self.session.find_element_by_id("LoginApps_Password").send_keys(self.userpwd)
+        self.session.find_element_by_id("LoginApps_btnlogin").click()
+    
+    def open_app(self):
+        wait(2)
+        self.session.find_element_by_id("MainContent_LVportalapps_ctrl0_imgLogo_App_0").click()
+        wait(2)
 
-users = dict()
-for line in credentials:
-    if line[0] == '#':
-        continue
-    u, p = line.split(',')
-    users[u] = p.strip() # removing \n characters
+    def entry_data(self):
+        self.session.get('https://www.bpocenter-dxc.com/hp_web2/es-corp/app/Jornada/Reg_jornada.aspx')
+        hstart = self.session.find_element_by_id("ctl00_Sustituto_d_hora_inicio1")
+        mstart = self.session.find_element_by_id("ctl00_Sustituto_D_minuto_inicio1")
+        hend = self.session.find_element_by_id("ctl00_Sustituto_d_hora_final1")
+        mend = self.session.find_element_by_id("ctl00_Sustituto_d_minuto_final1")
+        horas = self.session.find_element_by_id("ctl00_Sustituto_T_efectivo")
+        
+        # in case some values exist already, they need to be removed
+        hstart.send_keys(Keys.HOME+'09')
+        mstart.send_keys(Keys.HOME+'0')
+        hend.send_keys(Keys.HOME+'20')
+        mend.send_keys(Keys.HOME+'0')
+        horas.send_keys(Keys.BACKSPACE*5 + hours_value)
+
+    def save_quit(self):
+        self.session.find_element_by_id("ctl00_Sustituto_Btn_Guardar").click()
+        self.session.quit()
+
+
+######################## MAIN ##################################
+
+hours = calculate_hours()
+users = get_credentials()
 
 for each in users:
     username = each
     userpwd = users[each]
-
-# to work in silent mode
-opts = Options()
-opts.headless = True
-assert opts.headless  # Operating in headless mode
-
-# open login page, enter credentials
-browser = Firefox(options=opts)
-browser.get('https://www.bpocenter-dxc.com/iwom_web5/portal_apps.aspx')
-
-browser.find_element_by_id("LoginApps_UserName").send_keys(username)
-browser.find_element_by_id("LoginApps_Password").send_keys(userpwd)
-browser.find_element_by_id("LoginApps_btnlogin").click()
-browser.find_element_by_id("MainContent_LVportalapps_ctrl0_imgLogo_App_0").click()
-
-t1 = mktime(localtime())
-t2 = mktime(localtime())
-while t2-t1 < 2:   # wait 2 sec.
-    t2 = mktime(localtime())
-
-# open entry page, and introduce data
-browser.get('https://www.bpocenter-dxc.com/hp_web2/es-corp/app/Jornada/Reg_jornada.aspx')
-hstart = browser.find_element_by_id("ctl00_Sustituto_d_hora_inicio1")
-mstart = browser.find_element_by_id("ctl00_Sustituto_D_minuto_inicio1")
-hend = browser.find_element_by_id("ctl00_Sustituto_d_hora_final1")
-mend = browser.find_element_by_id("ctl00_Sustituto_d_minuto_final1")
-horas = browser.find_element_by_id("ctl00_Sustituto_T_efectivo")
-
-# in case some values exist already, they need to be removed
-hstart.send_keys(Keys.HOME+'09')
-mstart.send_keys(Keys.HOME+'0')
-hend.send_keys(Keys.HOME+'19')
-mend.send_keys(Keys.HOME+'0')
-horas.send_keys(Keys.BACKSPACE*5 + hours_value)
-
-# save, and close browser
-browser.find_element_by_id("ctl00_Sustituto_Btn_Guardar").click()
-browser.quit()
+    EnterHours('firefox', username, userpwd, hours)
