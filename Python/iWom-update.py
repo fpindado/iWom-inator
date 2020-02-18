a738ffe0837db1324d6898f7cdf073cbf48aaefa
osso73 <52031346+osso73@users.noreply.github.com>
2020-02-18 12:48:30 +0100

    Added logic for Fridays, and credentials file

--- Python/iWom-update.py
+++ Python/iWom-update.py
@@ -10,9 +10,32 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from time import asctime, mktime, localtime
 
+CREDENTIALS = "users.csv"
 
-NAME = "Oriol Pujol"
-PWD = "iWom-2019"
+# calculate hours
+weekday = asctime()[:3]
+if weekday == 'Fri':
+    hours_value = '6:30'
+elif weekday in ['Mon', 'Tue', 'Wed', 'Thu']:
+    hours_value = '8:32'
+else:
+    print ("You can't run the macro during weekends!")
+
+
+# get credentials from file
+with open (CREDENTIALS, encoding='utf-8-sig') as f:
+    credentials = f.readlines()
+
+users = dict()
+for line in credentials:
+    if line[0] == '#':
+        continue
+    u, p = line.split(',')
+    users[u] = p.strip() # removing \n characters
+
+for each in users:
+    username = each
+    userpwd = users[each]
 
 # to work in silent mode
 opts = Options()
@@ -23,18 +46,17 @@ assert opts.headless  # Operating in headless mode
 browser = Firefox(options=opts)
 browser.get('https://www.bpocenter-dxc.com/iwom_web5/portal_apps.aspx')
 
-browser.find_element_by_id("LoginApps_UserName").send_keys(NAME)
-browser.find_element_by_id("LoginApps_Password").send_keys(PWD)
+browser.find_element_by_id("LoginApps_UserName").send_keys(username)
+browser.find_element_by_id("LoginApps_Password").send_keys(userpwd)
 browser.find_element_by_id("LoginApps_btnlogin").click()
 browser.find_element_by_id("MainContent_LVportalapps_ctrl0_imgLogo_App_0").click()
 
-# open entry page, and introduce data
 t1 = mktime(localtime())
 t2 = mktime(localtime())
 while t2-t1 < 2:   # wait 2 sec.
     t2 = mktime(localtime())
 
-
+# open entry page, and introduce data
 browser.get('https://www.bpocenter-dxc.com/hp_web2/es-corp/app/Jornada/Reg_jornada.aspx')
 hstart = browser.find_element_by_id("ctl00_Sustituto_d_hora_inicio1")
 mstart = browser.find_element_by_id("ctl00_Sustituto_D_minuto_inicio1")
@@ -45,9 +67,9 @@ horas = browser.find_element_by_id("ctl00_Sustituto_T_efectivo")
 # in case some values exist already, they need to be removed
 hstart.send_keys(Keys.HOME+'09')
 mstart.send_keys(Keys.HOME+'0')
-hend.send_keys(Keys.HOME+'18')
+hend.send_keys(Keys.HOME+'19')
 mend.send_keys(Keys.HOME+'0')
-horas.send_keys(Keys.BACKSPACE*5+'08:32')
+horas.send_keys(Keys.BACKSPACE*5 + hours_value)
 
 # save, and close browser
 browser.find_element_by_id("ctl00_Sustituto_Btn_Guardar").click()
